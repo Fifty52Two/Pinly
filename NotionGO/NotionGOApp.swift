d9698df4d5a0f106d6418c447b02dadae52f478a
Ferhat Akköprü <akkopru_ferhat65@outlook.com>
2026-02-01 17:38:30 +0300

    Initial Commit

---
+++ NotionGO/NotionGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotionGOApp.swift
+//  NotionGO
+//
+//  Created by Ferhat Akköprü on 1.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct NotionGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
